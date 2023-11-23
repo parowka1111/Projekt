ca5870316c3a560f982a7226110be6c39166be40
Jan kowalski <jan.kowalski@pollub.edu.pl>
2023-11-23 15:17:45 +0100

    komentarz1

---
+++ src/main/java/com/example/demo/models/Comment.java
@@ -0,0 +1,20 @@
+package com.example.demo.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+@Getter
+@Setter
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+    private User author;
+    private Boolean isSpoiler;
+
+}
