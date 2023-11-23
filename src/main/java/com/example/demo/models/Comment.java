a2d2192253ed85975de464adc8b94a9d5cb60693
Jan kowalski <jan.kowalski@pollub.edu.pl>
2023-11-23 15:45:44 +0100

    kont2

--- src/main/java/com/example/demo/models/Comment.java
+++ src/main/java/com/example/demo/models/Comment.java
@@ -4,8 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-@Getter
-@Setter
+
 @Entity
 @Table(name = "comment")
 public class Comment {
@@ -17,5 +16,37 @@ public class Comment {
     @ManyToOne
     private User author;
     private Boolean isSpoiler;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Boolean getIsSpoiler() {
+        return isSpoiler;
+    }
+
+    public void setIsSpoiler(Boolean isSpoiler) {
+        this.isSpoiler = isSpoiler;
+    }
 
 }
