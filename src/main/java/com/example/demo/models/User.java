63e16c07e096465743babaf2ed644f3b6fff6bc8
Jan kowalski <jan.kowalski@pollub.edu.pl>
2023-11-23 15:13:25 +0100

    Dodanie User klasy

---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,40 @@
+package com.example.demo.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String name) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
