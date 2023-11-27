d81dceb604ac4d602f3e00d340648e63621ce348
Jan kowalski <jan.kowalski@pollub.edu.pl>
2023-11-27 14:31:42 +0100

    poczatek logowania

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -36,6 +38,26 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public Long authenticateUser(String usernameOrEmail, String password) {
+        Long userId;
+
+        if (usernameOrEmail.contains("@")) {
+            userId = getIdByName(usernameOrEmail, "email");
+        } else {
+            userId = getIdByName(usernameOrEmail, "username");
+        }
+
+
+        Optional<User> userOptional = userRepository.findById(userId);
+        User user = userOptional.orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!password.equals(user.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+
+        return user.getId();
+    }
+
 
     private boolean isPasswordValid(String password) {
         Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
@@ -57,6 +79,18 @@ public class UserService {
         if (count > 0) {
             throw new IllegalArgumentException(fieldName + " exists");
         }
+    }
+    private Long getIdByName(String value, String fieldName) {
+        String jpql = "SELECT u.id FROM User u WHERE u." + fieldName + " = :value";
+        Query query = entityManager.createQuery(jpql);
+        query.setParameter("value", value);
+
+        List<Long> userIds = query.getResultList();
+
+        if (userIds.isEmpty()) {
+            throw new IllegalArgumentException(fieldName + "not found");
+        }
+        return userIds.get(0);
     }
 
     private String assignRole(String email) {
